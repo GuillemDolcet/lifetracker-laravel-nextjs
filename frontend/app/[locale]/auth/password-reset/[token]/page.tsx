29ab5a21ed8baf0b288debe8329bf5e6909274d9
Guillem Dolcet <guillem.dolcet@tuv.com>
2025-02-04 17:23:06 +0100

    move password reset form to a component

--- frontend/app/[locale]/auth/password-reset/[token]/page.tsx
+++ frontend/app/[locale]/auth/password-reset/[token]/page.tsx
@@ -1,105 +1,32 @@
 'use client'
 
-import Button from '@/components/Button'
-import Input from '@/components/Input'
-import InputError from '@/components/InputError'
-import Label from '@/components/Label'
-import { useAuth } from '@/hooks/auth'
-import { useEffect, useState } from 'react'
-import { useSearchParams } from 'next/navigation'
+import {useTranslations} from "next-intl"
+import CardTitle from "@/components/CardTitle";
+import CardDescription from "@/components/CardDescription";
+import {Link} from "@/i18n/routing";
+import {useAuth} from "@/hooks/auth";
+import PasswordResetForm from "@/components/forms/PasswordResetForm";
 
-const PasswordReset = () => {
-    const searchParams = useSearchParams()
+const Page = () => {
+    const { forgotPassword } = useAuth({
+        middleware: 'guest',
+        redirectIfAuthenticated: '/admin',
+    })
 
-    const { resetPassword } = useAuth({ middleware: 'guest' })
-
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [passwordConfirmation, setPasswordConfirmation] = useState('')
-    const [errors, setErrors] = useState([])
-
-    const submitForm = event => {
-        event.preventDefault()
-
-        resetPassword({
-            email,
-            password,
-            password_confirmation: passwordConfirmation,
-            setErrors,
-        })
-    }
-
-    useEffect(() => {
-        setEmail(searchParams.get('email'))
-    }, [searchParams.get('email')])
+    const translations = useTranslations('Auth')
 
     return (
         <>
-            <form onSubmit={submitForm}>
-                {/* Email Address */}
-                <div>
-                    <Label htmlFor="email">Email</Label>
-
-                    <Input
-                        id="email"
-                        type="email"
-                        value={email}
-                        className="block mt-1 w-full"
-                        onChange={event => setEmail(event.target.value)}
-                        required
-                        autoFocus
-                    />
-
-                    <InputError messages={errors.email} className="mt-2" />
+            <div className="container-tight">
+                <div className="card card-md">
+                    <div className="card-body">
+                        <CardTitle>{translations('reset_password')}</CardTitle>
+                        <PasswordResetForm submitRequest={forgotPassword}/>
+                    </div>
                 </div>
-
-                {/* Password */}
-                <div className="mt-4">
-                    <Label htmlFor="password">Password</Label>
-                    <Input
-                        id="password"
-                        type="password"
-                        value={password}
-                        className="block mt-1 w-full"
-                        onChange={event => setPassword(event.target.value)}
-                        required
-                    />
-
-                    <InputError
-                        messages={errors.password}
-                        className="mt-2"
-                    />
-                </div>
-
-                {/* Confirm Password */}
-                <div className="mt-4">
-                    <Label htmlFor="passwordConfirmation">
-                        Confirm Password
-                    </Label>
-
-                    <Input
-                        id="passwordConfirmation"
-                        type="password"
-                        value={passwordConfirmation}
-                        className="block mt-1 w-full"
-                        onChange={event =>
-                            setPasswordConfirmation(event.target.value)
-                        }
-                        required
-                    />
-
-                    <InputError
-                        messages={errors.password_confirmation}
-                        className="mt-2"
-                    />
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    <Button>Reset Password</Button>
-                </div>
-            </form>
+            </div>
         </>
     )
 }
 
-export default PasswordReset
+export default Page
